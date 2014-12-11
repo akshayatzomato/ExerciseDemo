0f46ae0314a2dd5f27bff331afae3ff9ebf1b1f0
Akshay Chugh <akshay.chugh@zomato.com>
2014-12-11 16:44:19 +0530

    dump hotel Ids

--- index.php
+++ index.php
@@ -1,53 +1,17 @@
 <?php
 
-
 include_once __DIR__ . '/includes/application_top.php';
 
+# Setting up curl request
 $url = 'http://deals.expedia.com/beta/deals/hotels.json';
 $session = curl_init( $url );                                           
 curl_setopt( $session, CURLOPT_RETURNTRANSFER, true );                            
 $json = curl_exec( $session );                                                   
-$phpObj = json_decode( $json );
+$hotel_deals = json_decode( $json, true );
 
-var_dump( $phpObj );
-die();
-
-
-/*
-require __DIR__ . '/vendor/autoload.php';
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-  'twig.path' => __DIR__.'/views',
-));
-
-$app->get('/twig/{name}', function ($name) use ($app) {
-    return $app['twig']->render('index.twig', array(
-        'name' => $name,
-    ));
-});
-
-$dbopts = parse_url(getenv('DATABASE_URL'));
-$app->register(new Herrera\Pdo\PdoServiceProvider(),
-  array(
-    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
-    'pdo.port' => $dbopts["port"],
-    'pdo.username' => $dbopts["user"],
-    'pdo.password' => $dbopts["pass"]
-  )
-);
+# Deal with output
+foreach ( $hotel_deals as $deal ) {
+    echo $deal['hotelId'] . PHP_EOL;
+}
 
 
-$app->get('/db/', function() use($app) {
-  $st = $app['pdo']->prepare('SELECT name FROM test_table');
-  $st->execute();
-
-  $names = array();
-  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-    $app['monolog']->addDebug('Row ' . $row['name']);
-    $names[] = $row;
-  }
-
-  return $app['twig']->render('database.twig', array(
-    'names' => $names
-  ));
-});
-*/

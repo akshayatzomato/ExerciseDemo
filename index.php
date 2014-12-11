3c22047b6a047c578502f4abef11a0fc3055a1dc
Akshay Chugh <akshay.chugh@zomato.com>
2014-12-11 15:26:26 +0530

    fixing include path for autoload.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require('vendor/autoload.php');
+require __DIR__ . '/vendor/autoload.php';
 echo "Hello World";
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path' => __DIR__.'/../views',

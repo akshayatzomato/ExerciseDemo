feb7b8cb0c844c2727968e38f9955b0f42269918
chughakshay16 <chughakshay16@gmail.com>
2014-12-15 18:55:41 +0530

    Removed simpletest; added phpunit test cases

--- bootstrap.php
+++ bootstrap.php
@@ -1,2 +1,9 @@
 <?php
-
+/*                                                                                 
+ * PHPUnit bootstrap script.                                                       
+ * Add any test suite related configuration here.                                  
+ */                                                                                
+                                                                                   
+require __DIR__ . '/includes/start.php';
+                                                                                   
+?>
--- test/global_function_test.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/simpletest/autorun.php';
-
-class TestGlobal extends UnitTestCase {
-    function test        
-}
